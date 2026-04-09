22d7443aaea8d6fa175800079364da563c28521a
Yash <yash7singh3@gmail.com>
2026-04-09 22:44:23 +0530

    Pipeline Complition

---
+++ app.py
@@ -0,0 +1,90 @@
+import sys
+import os
+
+import certifi
+ca = certifi.where()
+
+from dotenv import load_dotenv
+load_dotenv()
+mongo_db_url = os.getenv("MONGO_DB_URL")
+print(mongo_db_url)
+
+import pymongo
+from packetping.exception.exception import NetworkSecurityException
+from packetping.logging.logger import logging
+from packetping.pipeline.training_pipeline import TrainingPipeline
+from packetping.utils.ml_utils.model.estimator import NetworkModel
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile, Request
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
+import pandas as pd
+
+from packetping.utils.main_utils.utils import load_object
+
+client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+
+from packetping.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
+from packetping.constant.training_pipeline import DATA_INGESTION_DATABASE_NAME
+
+database = client[DATA_INGESTION_DATABASE_NAME]
+collection = database[DATA_INGESTION_COLLECTION_NAME]
+
+app = FastAPI()
+orgins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=orgins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def train_route():
+    try:
+        train_pipeline = TrainingPipeline()
+        train_pipeline.run_pipeline()
+        return Response("Training is successful!!")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+@app.post("/predict")
+async def predict_route(request: Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df["Predicted_Column"] = y_pred
+        print(df["Predicted_Column"])
+
+        df.to_csv("prediction_output/output.csv")
+
+        table_html = df.to_html(classes='table table-striped')
+        return templates.TemplateResponse(
+                                            request,
+                                            "table.html",
+                                            {"table": table_html}
+                                        )
+    
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+if __name__ == "__main__":
+    app_run(app, host="localhost", port=8000)
